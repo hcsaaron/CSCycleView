5f365c2277a4ce7f061a23dbdacde5a3a34154ce
Aaron <hcsaaron@163.com>
2019-01-12 21:16:36 +0800

    Initial Commit

---
+++ CSCycleView/CSCycleView.h
@@ -0,0 +1,19 @@
+//
+//  CSCycleView.h
+//  CSCycleView
+//
+//  Created by 黄楚升 on 2019/1/12.
+//  Copyright © 2019年 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSCycleView.
+FOUNDATION_EXPORT double CSCycleViewVersionNumber;
+
+//! Project version string for CSCycleView.
+FOUNDATION_EXPORT const unsigned char CSCycleViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSCycleView/PublicHeader.h>
+
+
